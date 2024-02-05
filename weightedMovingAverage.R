1c157d5e0d5d6ee553e1410f45e70b69c0d21488
Max Gotts <maxgotts@dynamic-oit-vapornet100-10-9-74-205.princeton.edu>
2024-02-05 16:36:06 -0500

    Example code done, wma now correct too

--- weightedMovingAverage.R
+++ weightedMovingAverage.R
@@ -1,4 +1,8 @@
-gauss <- function(x, mean, std) return (exp(-0.5*((x - mean)/std)^2)) # Guassian kernel
+gauss <- function(x, center, width) return(exp(-0.5*((x - center)/width)^2)) # Gaussian kernel
+rect <- function(x, center, width) return(as.numeric(abs(x-center)<width/2)) # Rectangular kernel for simple moving average
+hann <- function(x, center, width) return((1/width) * cos(pi*(x-center)/width)^2  * rect(x, center, width)) # Hann kernel
+
+
 
 weightedMovingAverage <- function(dat, axes, width, kernel=gauss, alpha=0.05, spine=NULL) {
   dat <- as.data.frame(dat) # Convert the given data to be a dataframe
@@ -6,9 +10,18 @@ weightedMovingAverage <- function(dat, axes, width, kernel=gauss, alpha=0.05, sp
   x.name <- as.character(axes$x) # Get the name of the x-axis, over which we will do the moving average
   y.name <- as.character(axes$y) # Get the name of the y-axis, that which will be averaged
   by.names <- as.character(unlist(axes)[setdiff(names(axes), c("x","y"))]) # Get the remaining names of the axes used to split the dataset
-  if (is.null(spine)) spine <- seq(from=min(dat[,x.name]), to=max(dat[,x.name]), length.out=1000) # If none are provided, generate a new x-axis which will form the backbone of the average
+  spineNULL <- FALSE # Record that spine was not NULL
+  if (is.null(spine)) {
+    spineNULL <- TRUE # Record that spine was set to NULL
+    spine <- seq(from=min(dat[,x.name]), to=max(dat[,x.name]), length.out=1000) # If none are provided, generate a new x-axis which will form the backbone of the average
+  }
   
   if (length(by.names)==0) { # If there are no splits provided in axes
+    if (nrow(dat)==0) { # If empty dataset has been provided, return an empty dataframe
+      wma.dat <- as.data.frame(matrix(NA,nrow=0,ncol=5))
+      colnames(wma.dat) <- c(y.name,"SD","Neff","SE")
+      return(wma.dat)
+    }
     wma.dat <- data.frame(x=spine) # Create main dataframe filled with the spine
     colnames(wma.dat) <- x.name # Name the new dataframe as the x-axis
     for (xIndex in 1:length(spine)) { # Iterate through steps in spine
@@ -20,7 +33,7 @@ weightedMovingAverage <- function(dat, axes, width, kernel=gauss, alpha=0.05, sp
       Neff <- 1/SumOfSquares # Calculate effective N of the kernel based on sum-of-squares
       SD <- sqrt((sum(kernel.value*(dat[,y.name]-Average)^2))/(1-SumOfSquares)) # Get standard deviation of convolution
       SE <- z*SD/sqrt(Neff) # Calculate standard error using standard deviation, z-score, and effective N
-      wma.dat[xIndex, c("Average", "SD", "Neff", "SE")] <- c(Average, SD, Neff, SE) # Add relevant values to main dataframe
+      wma.dat[xIndex, c(y.name, "SD", "Neff", "SE")] <- c(Average, SD, Neff, SE) # Add relevant values to main dataframe
     }
   } else { 
     # Generate a dataframe of all combinations of split columns' values
@@ -35,9 +48,11 @@ weightedMovingAverage <- function(dat, axes, width, kernel=gauss, alpha=0.05, sp
       for (col in 1:length(by.names)) { # For each split value...
         sub <- sub[sub[,by.names[col]]==rows.of.values[row,col],] # ...subset the sub dataset by that value
       }
+      newspine <- spine # New spine that will be sent to the recursive step below
+      if (spineNULL) newspine <- NULL # If spine was originally NULL, let it continue to be so
       wma.add <- weightedMovingAverage(sub, list(x=x.name, y=y.name),
                                        width, kernel=kernel, alpha=alpha,
-                                       spine=spine
+                                       spine=newspine
                 ) # Perform the recursive step with all values the same except the axes to create a to-add dataframe
       wma.add[,by.names] <- rows.of.values[row,by.names] # Add split column values by name
       wma.dat <- rbind(wma.dat, wma.add) # Add the recursive to-add dataframe to the main dataframe
